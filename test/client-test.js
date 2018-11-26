7d6c349913f7e4326c22a7e52fb3c84dbfb2779a
Mortem45 <ste45_007@hotmail.com>
2018-11-26 11:57:36 -0600

    mock apps

--- test/client-test.js
+++ test/client-test.js
@@ -2,6 +2,20 @@
 
 const test = require('ava')
 const cloninstagram = require('../')
+const fixtures = require('./fixtures')
+const nock = require('nock')
+
+let options = {
+  endpoints: {
+    pictures: 'http://cloninstagram.test/picture',
+    users: 'http://cloninstagram.test/user',
+    auth: 'http://cloninstagram.test/auth'
+  }
+}
+
+test.beforeEach(t => {
+  t.context.client = cloninstagram.createClient(options)
+})
 
 test('client', t => {
   const client = cloninstagram.createClient()
@@ -14,4 +28,17 @@ test('client', t => {
   t.is(typeof client.saveuser, 'function')
   t.is(typeof client.getUser, 'function')
   t.is(typeof client.auth, 'function')
+})
+
+test('getPicture', async t => {
+  const client = t.context.client
+
+  let image = fixtures.getImage()
+
+  nock(options.endpoints.pictures)
+    .get(`/${image.publicId}`)
+    .reply(200, image)
+
+  let result = await client.getPicture(image.publicId)
+  t.deepEqual(image, result)
 })
